14bb536f23700d555224a7aefacb6d49ea9e2478
Cesar Soares Stenico <32494389+cesarsst@users.noreply.github.com>
2018-12-19 09:52:50 -0200

    Edit 01
    Remoção de arquivos inuteis

---
+++ PythonTeste/venv/Aula06b.py
@@ -0,0 +1,5 @@
+n = (input('Digite algo:'))
+print(n.isnumeric()) #N é numerico? True or False
+print(n.isalpha()) #É alfabetico? True or False
+print(n.isalnum()) #É alfanumerico? True or False
+print(n.isupper()) #É tudo maiusculo? True or False
---
+++ PythonTeste/venv/Aula08b.py
@@ -0,0 +1,16 @@
+#Gerando numero aleatorio (entre 0 e 1)
+import random
+num = random.random()
+print(num)
+
+num1 = random.randint(1,10) #gera numero entre 1 e 10
+print(num1)
+
+# Para importar novas biblioteca clica ctrl+alt+S vá até Projects> Project Interpreter -> clico no + e adicione
+import emoji
+print(emoji.emojize('Hello world :earth_americas:', use_aliases=True))
+
+
+
+
+
